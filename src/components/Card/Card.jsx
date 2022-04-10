8bfffa60e62a342a3378f96f7085405ff14c0da8
Glebkas <baran123456@gmail.com>
2022-04-10 22:27:49 +0300

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,19 +5,33 @@ import { CardList } from './CardList';
 import { CardListItem } from './CardListItem';
 import { CardTitle } from './CardTitle';
 
-export const Card = (img, name, info = [], onClick) => {
+export const Card = (props) => {
+    let info = [
+        {
+            title: 'Population',
+            description: props.country.population,
+        },
+        {
+            title: 'Region',
+            description: props.country.region,
+        },
+        {
+            title: 'Capital',
+            description: props.country.capital,
+        },
+    ];
+
     return (
         <CardWrapper>
-            <CardImage></CardImage>
+            <CardImage src={props.country.flags.png}></CardImage>
             <CardBody>
-                <CardTitle>{name}</CardTitle>
+                <CardTitle>{props.country.name}</CardTitle>
                 <CardList>
                     {info.map((el) => (
                         <CardListItem key={el.title}>
                             <b>{el.title}:</b> {el.description}
                         </CardListItem>
                     ))}
-                    ;
                 </CardList>
             </CardBody>
         </CardWrapper>
