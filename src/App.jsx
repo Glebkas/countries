0ae72d7c3014742bb06eb08e60b4a3829919fc93
Glebkas <baran123456@gmail.com>
2022-04-30 22:32:00 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Header } from './components/Header/Header';
 import { Main } from './components/Main/Main';
 import { HomePage } from './pages/HomePage';
@@ -6,12 +8,23 @@ import { NotFound } from './pages/Notfound';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
+    const [countries, setCountries] = useState([]);
+
     return (
         <>
             <Header />
             <Main>
                 <Routes>
-                    <Route path='/' element={<HomePage />}></Route>
+                    <Route
+                        index
+                        path='/'
+                        element={
+                            <HomePage
+                                countries={countries}
+                                setCountries={setCountries}
+                            />
+                        }
+                    ></Route>
                     <Route path='/country/:name' element={<Details />} />
                     <Route path='*' element={<NotFound />} />
                 </Routes>
