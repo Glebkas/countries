a8a695f1b3267ed023b4fcf7e9eb9c5896474d95
Glebkas <baran123456@gmail.com>
2022-05-01 19:26:07 +0300

    Create InfoTitle.jsx

---
+++ src/components/Info/InfoTitle.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const InfoTitle = styled.h2`
+    margin: 0;
+    font-weight: var(--fw-bold);
+    font-size: var(--fs-dsvbg);
+    line-height: 30px;
+
+    @media (max-width: 767px) {
+        font-size: var(--fs-mvbg);
+    } ;
+`;
