ba8693d80832c9bf8e7859b89164622e531dedf4
Glebkas <baran123456@gmail.com>
2022-05-01 19:25:05 +0300

    Update CardTitle.jsx

--- src/components/Card/CardTitle.jsx
+++ src/components/Card/CardTitle.jsx
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const CardTitle = styled.h3`
     margin: 0;
-    font-size: var(--fs-md);
+    font-size: var(--fs-mbg);
     font-weight: var(--fw-bold);
 `;
