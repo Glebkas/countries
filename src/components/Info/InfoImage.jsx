f06cc9d07cfbf7b0321d121a4c84f29707cc72a8
Glebkas <baran123456@gmail.com>
2022-05-01 19:26:23 +0300

    Create InfoImage.jsx

---
+++ src/components/Info/InfoImage.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const InfoImage = styled.img`
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    border-radius: 10px;
+`;
