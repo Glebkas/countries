61e0b49ddf0c766b2e3823ab2a33d4772e3cdd63
Glebkas <baran123456@gmail.com>
2022-05-01 19:26:12 +0300

    Create InfoTag.jsx

---
+++ src/components/Info/InfoTag.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const InfoTag = styled.span`
+    padding: 6px 30px;
+    background-color: var(--color-ui-base);
+    box-shadow: var(--shadow);
+    line-height: 1.5;
+    cursor: pointer;
+`;
