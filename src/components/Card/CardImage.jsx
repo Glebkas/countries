bd1acc2e8854758422fb990f989cbeab99aaa46b
Glebkas <baran123456@gmail.com>
2022-05-01 19:24:59 +0300

    Update CardImage.jsx

--- src/components/Card/CardImage.jsx
+++ src/components/Card/CardImage.jsx
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 export const CardImage = styled.img`
     display: block;
     width: 100%;
-    height: 150px;
-    object-fit: cover;
+    object-fit: contain;
     object-position: center;
     box-shadow: var(--shadow);
 `;
