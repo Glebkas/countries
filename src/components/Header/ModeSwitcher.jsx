6fa38e93930af1322487323244a47e3160e3eb98
Glebkas <baran123456@gmail.com>
2022-05-01 19:25:13 +0300

    Update ModeSwitcher.jsx

--- src/components/Header/ModeSwitcher.jsx
+++ src/components/Header/ModeSwitcher.jsx
@@ -2,10 +2,14 @@ import styled from 'styled-components';
 
 export const ModeSwitcher = styled.div`
     color: var(--colors-text);
-    font-size: var(--fs-vsm);
+    font-size: var(--fs-md);
     cursor: pointer;
     font-weight: var(--fw-normal);
     display: flex;
     gap: 0.5rem;
     text-transform: capitalize;
+
+    @media (max-width: 767px) {
+        font-size: var(--fs-vsm);
+    }
 `;
