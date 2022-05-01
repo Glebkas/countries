d827d2d34a61d1dee4d4baae6c1487a3f4e0ccd3
Glebkas <baran123456@gmail.com>
2022-05-01 19:25:07 +0300

    Update CardWrapper.jsx

--- src/components/Card/CardWrapper.jsx
+++ src/components/Card/CardWrapper.jsx
@@ -4,5 +4,9 @@ export const CardWrapper = styled.article`
     border-radius: var(--radii);
     background-color: var(--colors-ui-base);
     cursor: pointer;
-    overflow: hiden;
+    overflow: hidden;
+
+    @media (max-width: 767px) {
+        margin: 0 30px;
+    } ;
 `;
