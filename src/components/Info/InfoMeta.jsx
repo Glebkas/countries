371cadd265b31f33333e090cea5c926b14d9b302
Glebkas <baran123456@gmail.com>
2022-05-01 19:26:16 +0300

    Create InfoMeta.jsx

---
+++ src/components/Info/InfoMeta.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const InfoMeta = styled.div`
+    margin-top: 3rem;
+    display: flex;
+    gap: 1.5rem;
+    flex-direction: column;
+    align-items: flex-start;
+
+    & > b {
+        font-weight: var(--fw-bold);
+    }
+
+    @media (min-size: 767px) {
+        flex-direction: row;
+        align-items: center;
+    }
+`;
